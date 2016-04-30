1ef1d8ff09b3ba5127d943aca72561a9c678dfe9
Kevin McKernan <kevin@mckernan.in>
2016-04-30 08:27:49 -0600

    theme functions to class, add tahosa button shortcode from old theme

--- functions.php
+++ functions.php
@@ -1,35 +1,52 @@
 <?php
-function cc_mime_types( $mimes ) {
-	$mimes['svg'] = 'image/svg+xml';
 
-	return $mimes;
-}
-add_filter( 'upload_mimes', 'cc_mime_types' );
+class TahosaLodgeTheme {
 
-/**
- * TypeKit Fonts.
- *
- * @since Theme 1.0
- */
-function theme_typekit() {
-	wp_enqueue_script( 'theme_typekit', '//use.typekit.net/ihq0jhu.js' );
-}
-add_action( 'wp_enqueue_scripts', 'theme_typekit' );
+	function __construct() {
 
-function theme_typekit_inline(){
-	if ( wp_script_is( 'theme_typekit', 'done' ) ) {
-		?>
-	  <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
-<?php
+		require_once( 'inc/tahosa-button.php' );
+
+		add_filter( 'upload_mimes', array( $this, 'mime_types' ) );
+		add_filter( 'body_class',   array( $this, 'add_slug_body_class' ) );
+		add_action( 'send_headers', array( $this, 'custom_headers' ) );
+
+		$this->roots_support();
+	}
+
+	public function mime_types( $mimes ) {
+		$mimes['svg'] = 'image/svg+xml';
+
+		return $mimes;
+	}
+
+	public function add_slug_body_class( $classes ) {
+		global $post;
+		if ( isset( $post ) ) {
+			$classes[] = $post->post_type . '-' . $post->post_name;
+		}
+		return $classes;
+	}
+
+	public function custom_headers() {
+		header( 'Access-Control-Allow-Origin: *' );
+	}
+
+	public function roots_support() {
+		add_theme_support( 'soil-clean-up' );
+		add_theme_support( 'soil-disable-asset-versioning' );
+		add_theme_support( 'soil-disable-trackbacks' );
+		// add_theme_support( 'soil-google-analytics', '' );
+		add_theme_support( 'soil-jquery-cdn' );
+		// add_theme_support( 'soil-js-to-footer' );
+		add_theme_support( 'soil-nav-walker' );
+		add_theme_support( 'soil-nice-search' );
+		add_theme_support( 'soil-relative-urls' );
+	}
+
+	public function theme_typekit() {
+		wp_enqueue_script( 'theme_typekit', 'https://use.typekit.net/xbk1ivk.js' );
+		wp_inline_script( 'try{Typekit.load({ async: true });}catch(e){}' );
 	}
 }
-add_action( 'wp_head', 'theme_typekit_inline' );
 
-function add_body_class( $classes ) {
-    global $post;
-    if ( isset( $post ) ) {
-        $classes[] = $post->post_type . '-' . $post->post_name;
-    }
-    return $classes;
-}
-add_filter( 'body_class', 'add_body_class' );
+new Wimachtendienk();
